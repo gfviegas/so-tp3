0f205076f2441ce5875657845404bffb693248ee
Gustavo Viegas <webviegas@gmail.com>
2019-06-05 02:03:11 -0300

    Fazendo exemplo de comunicação com a API usando o axios

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, Blueprint
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
-api = Api(app)
+bp = Blueprint('api', __name__)
+api = Api(bp)
 
 class HelloWorld(Resource):
     def get(self):
-        return jsonify({'hello': 'world'})
+        return {'hello': 'world'}, 200
+
 
 api.add_resource(HelloWorld, '/')
+app.register_blueprint(bp, url_prefix='/api')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
--- frontend/src/components/home/home.js
+++ frontend/src/components/home/home.js
@@ -1,19 +1,44 @@
-import React from 'react'
+import React, { Component} from 'react'
+import axios from 'axios'
 
 // Assets
 import logo from '../../assets/images/logo.svg'
 import './home.sass'
 
-function Home () {
-  return (
-    <div className='app'>
-      <header className='app-header'>
-        <img src={logo} className='app-logo' alt='logo' />
-        <p> Edit <code>src/App.js</code> and save to reload. </p>
-        <a className='app-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'> Learn React </a>
-      </header>
-    </div>
-  )
+class Home extends Component {
+  state = { message: '' }
+
+  async componentDidMount () {
+    try {
+      const message = JSON.stringify(await this.callApi())
+      this.setState({message})
+    } catch (e) {
+      console.error(e)
+      this.setState({message: 'Erro na API!'})
+    }
+  }
+
+  async callApi () {
+    console.info('Call APi called')
+    const response = await axios.get('/api/')
+    const body = response.data
+
+    if (response.status !== 200 && response.status !== 204) throw Error(body.message)
+
+    return body
+  }
+
+  render () {
+    return (
+      <div className='app'>
+        <header className='app-header'>
+          <img src={logo} className='app-logo' alt='logo' />
+          <p> Mensagem da API: {this.state.message} </p>
+        </header>
+      </div>
+    )
+  }
 }
+
 
 export default Home
